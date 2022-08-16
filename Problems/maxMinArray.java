4c66bc0a8a0770c21bfe2a6df9ce2a09dfcd1be8
Nabendu Das <nabendudas2001@gmail.com>
2022-08-16 09:13:04 +0530

    added max min function

---
+++ Problems/maxMinArray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class maxMinArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0; i<a.length; i++){
+            a[i] = sc.nextInt();
+        }
+        sumMaxMin(a);
+   } 
+   static void sumMaxMin(int a[]){
+    int max = a[0];
+    int min = a[0];
+    int sum = 0;
+    for(int i=0; i<a.length; i++){
+        if(max<a[i]){
+            max = a[i];
+        }
+        if(min>a[i]){
+            min = a[i];
+        }
+    }
+    sum = max + min;
+    System.out.println("Max element : "+max);
+    System.out.println("Min element : "+min);
+    System.out.println("Sum of Max and Min is : "+sum);
+   }
+}
