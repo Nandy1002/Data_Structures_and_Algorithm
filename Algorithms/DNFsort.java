1d7459b64018923c175a8b75c754460fe725226f
Nandy_X_Coder <nabendudas2001@gmail.com>
2022-08-22 19:14:43 +0530

    added two sorting algo for array

---
+++ Algorithms/DNFsort.java
@@ -0,0 +1,43 @@
+import java.util.Collection;
+
+/**
+ * Dutch National Flag Sort or Sort 012 algorithm
+ * this algorithm only apply for the array which has ony 3 elements
+ */
+public class DNFsort {
+    public static void main(String[] args) {
+        // the unsorted array of 0 1 2
+        int arr[] = { 1, 0, 2, 0, 1, 0, 1, 1, 2, 0 };
+        dnfSort(arr);
+        // after sort
+        for (int e : arr) {
+            System.out.print(e + " ");
+        }
+    }
+
+    // swapping function for array
+    static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // Dutch National Flag Algorithm
+    static void dnfSort(int arr[]) {
+        int low = 0;
+        int high = arr.length - 1;
+        int mid = 0;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+}
--- Algorithms/kadenesAlgorithm.java
+++ Algorithms/kadenesAlgorithm.java
@@ -1,6 +1,28 @@
-/* */
+/* 
+ * This algorithm is used to get the largest subarray sum in a contigious array
+ * Time Complexity - O(n)
+ * Space Complexity - O(1)  
+ */
 public class kadenesAlgorithm {
- public static void main(String[] args) {
-    
- }   
+    public static void main(String[] args) {
+        // given array
+        int a[] = { -1, 2, -2, 5, 7, -3, 1 };
+        System.out.println(maxSubArray(a));
+    }
+
+    // kadene's algorithm
+    static int maxSubArray(int a[]) {
+        int maxSum = 0;
+        int currentSum = 0;
+        for (int i = 0; i < a.length; i++) {
+            currentSum += a[i];
+            if (maxSum < currentSum) {
+                maxSum = currentSum;
+            }
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
 }
