5d95469ce02d832bd4eea96992ba3ef9d6d74560
Nandy_X_Coder <nabendudas2001@gmail.com>
2022-09-11 17:07:22 +0530

    added circular queue with array

---
+++ DataStructures/queueArray.java
@@ -0,0 +1,12 @@
+/*
+ * Circular Queue Data Structure with Array
+ */
+class QueueAR{
+    int array[] = new int[10];
+}
+public class queueArray {
+    public static void main(String[] args) {
+        
+    }
+    
+}
